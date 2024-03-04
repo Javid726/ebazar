55867cc8391a957cd2c1c964a591f8d7c2cfcc43
Javid Gasimov <javidgasimov98@gmail.com>
2024-03-04 17:05:44 +0400

    dashboard skeleton partly added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,16 @@
+import { Separator } from '@/components/ui/separator';
+import { Sidebar } from './sidebar';
+
 export default function DashboardPage() {
   return (
-    <div className="min-h-screen p-24 text-center bg-slate-200">
-      <h1 className="text-xl">Dashboard</h1>
+    <div className="min-h-screen flex gap-2">
+      <aside className="w-[200px]">
+        <Sidebar />
+      </aside>
+      <Separator orientation="vertical" />
+      <div className="grow rounded-lg bg-background shadow-lg border m-3 p-3 ">
+        DashboarddiSS
+      </div>
     </div>
   );
 }
