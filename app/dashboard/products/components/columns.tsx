86a859e2a2c47862dc954625e991442b786589fd
Javid Gasimov <javidgasimov98@gmail.com>
2024-04-02 01:50:35 +0400

    added fetching products and delete product

---
+++ app/dashboard/products/components/columns.tsx
@@ -0,0 +1,139 @@
+'use client';
+
+import { ColumnDef } from '@tanstack/react-table';
+
+import { MoreHorizontal, ArrowUpDown } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { Checkbox } from '@/components/ui/checkbox';
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Category = {
+  id: number;
+  name: string;
+  price: string;
+  discount_price: string;
+  quantity: number;
+  description: string;
+  parent_id: number;
+};
+
+// Define the type of your custom props
+type CustomColumnProps = {
+  customProps?: {
+    // Define your custom props here
+    onEditCategory: (category: Category) => void;
+    // Add other custom props if needed
+  };
+};
+
+// Extend ColumnDef with CustomColumnProps
+export type CustomColumnDef<T> = ColumnDef<T> & CustomColumnProps;
+
+export const columns: CustomColumnDef<Category & CustomColumnProps>[] & {
+  customProps?: any;
+} = [
+  {
+    id: 'select',
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && 'indeterminate')
+        }
+        onCheckedChange={value => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={value => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: 'name',
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
+        >
+          Məhsulun adı
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
+  {
+    accessorKey: 'price',
+    header: 'Qiymət',
+  },
+  {
+    accessorKey: 'quantity',
+    header: 'Sayı',
+  },
+  {
+    accessorKey: 'discount_price',
+    header: 'Endirimli qiyməti',
+  },
+  {
+    accessorKey: 'description',
+    header: 'Məhsul açıqlaması',
+  },
+  {
+    id: 'actions',
+    cell: ({ row, column }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Əməliyyatlar</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(String(payment.id))}
+            >
+              ID-ni kopyala
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() => {
+                column.columnDef.customProps.onEditCategory(payment);
+                column.columnDef.customProps.handleResetEdit();
+              }}
+            >
+              Redaktə et
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem
+              onClick={() =>
+                column.columnDef.customProps.handleOpen(payment.id)
+              }
+            >
+              Məhsulu sil
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -1,64 +1,139 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
+import { AlertDialogEl } from '../categories/components/AlertDialogEl';
 import { useContext, useEffect, useState } from 'react';
-import {
-  CategoryContext,
-  // CategoryDispatchContext,
-} from '@/app/category-provider';
-// import { CategoryDispatchContext } from '@/app/category-reducer';
+import { CategoryContext } from '@/app/category-provider';
 
 import { Loader2 } from 'lucide-react';
 import { toast } from 'sonner';
-import { Input } from '@/components/ui/input';
-import { Textarea } from '@/components/ui/textarea';
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from '@/components/ui/drawer';
-
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
 
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import EmptyProducts from './components/EmptyProducts';
+import { DataTable } from './components/data-table';
+import { columns } from './components/columns';
+import Link from 'next/link';
 
 export default function ProductsPage() {
+  const [products, setProducts] = useState([]);
+  const [renderPage, setRenderPage] = useState(false);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [deletedCategoryId, setDeletedCategoryId] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDeleteCategory = async () => {
+    setIsLoading(true);
+
+    const requestOptions: RequestInit = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('vendor_token')}`,
+      },
+      // mode: 'no-cors',
+    };
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/vendor/products/${deletedCategoryId}`,
+        requestOptions,
+      );
+      const result = await response.json();
+
+      if (response.ok) {
+        setIsLoading(false);
+
+        toast('Məhsul uğurla silindi!', {
+          action: {
+            label: 'Ok',
+            onClick: () => {
+              // setOpen(false);
+            },
+          },
+        });
+        setRenderPage(prevValue => !prevValue);
+      } else {
+        if (result.message) {
+          toast.error(result.message, {});
+        } else {
+          toast.error('Daxil etdiyiniz məlumatlarda yanlışlıq var');
+        }
+        setIsLoading(false);
+      }
+    } catch (error) {
+      toast.error('Gözlənilməyən xəta yarandı.', {
+        description: 'Zəhmət olmasa başqa vaxt yenidən cəhd edin..',
+        action: {
+          label: 'Ok',
+          onClick: () => {
+            // setOpen(false);
+          },
+        },
+      });
+      setIsLoading(false);
+    }
+
+    setShowDeleteDialog(false);
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      const requestOptions: RequestInit = {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('vendor_token')}`,
+        },
+      };
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/vendor/products`,
+          requestOptions,
+        );
+        const data = await response.json();
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+        setProducts([]);
+      }
+    };
+
+    getData();
+  }, [renderPage]);
+
   return (
     <div>
       <div className="my-2 flex justify-between">
         <h1 className="text-2xl font-semibold tracking-tight">Məhsullar</h1>
-        {/* <Button
-        type="button"
-        className="mr-4 font-medium text-md"
-        onClick={() => {
-          Form.reset();
-          setOpen(true);
-          handleSetEdit();
-        }}
-      >
-        Kateqoriya əlavə et
-      </Button> */}
+        <Link href="/dashboard/products/add-product">
+          <Button type="button" className="mr-4 font-medium text-md">
+            Məhsul əlavə et
+          </Button>
+        </Link>
       </div>
       <div className="mt-20">
-        <EmptyProducts />
+        {/* <EmptyProducts /> */}
+        <DataTable
+          data={products}
+          columns={columns}
+          onEditProduct={() => console.log('stick to the carate')}
+          handleOpen={(id: any) => {
+            setShowDeleteDialog(true);
+            setDeletedCategoryId(id);
+          }}
+        />
       </div>
+      <AlertDialogEl
+        open={showDeleteDialog}
+        handleClose={() => setShowDeleteDialog(false)}
+        handleDeleteCategory={handleDeleteCategory}
+      />
     </div>
   );
 }
